3dda79cfc1b01a5ad3c1d3f63048576f080e3ef4
oarcenth <=>
2020-04-16 14:47:16 -0700

    almost there

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -15,34 +15,83 @@
 
 //STEP 2
 
-// var sentence = window.prompt("say something all in lowercase");
-
-// function capsLock (str) { //takes a string from sentence
-//     var wordArray, i, final 
-
-//     wordArray = str.split(" "); 
-//     for (i = 0; i < wordArray.length; i ++) {
-//         wordArray[i][0].toUpperCase();  
-        
-//     };
-//     final = wordArray.join(" "); 
-    
-// };
-
-// window.console.log(capsLock(sentence)); 
-
-var name = ["robber", "de", "niro"]; 
-var newName = name.join(" "); 
-window.console.log(newName); 
-
-// var thingy = ["robert", "brad", "johnny"];
-// var i; 
-// for (i = 0 ; i < thingy.length; i ++) {
-// window.console.log(thingy[i][0].toUpperCase()); 
-// };
+// var words;
+// function upperCase(words) {
+//     "use strict";
+//     words = window.prompt("Please enter sentence");
+  
+//     //Take value from entry - and use regular expression to replace lowercase with and use a function with the upperCase() method 
+//     // \b finds certain character at THE BEGINNING OF EACH WORD
+//     // [a-z] means all lowercase functions 
+//     words = words.toLowerCase().replace(/\b[a-z]/g, function (letter) {
+//         return letter.toUpperCase();
+//     });
+//     return words;
+// }
+// window.console.log("Capitalizing the first letter of each world returns: " + upperCase(words));
 
 //STEP 3
 
+// var words;
+
+// function findVowels(words) {
+//     "use strict"; 
+//     words = window.prompt("Enter a sentence"); 
+//     // use the match method to find each instance using a reg ex as paramater
+//     // use regular expression [aeiou]/g for the vowels 
+//     // use .lenght to count the instances 
+//     words = words.match(/[aeiou]/g).length;
+//     return words; 
+
+// }
+
+// window.console.log("There are  " + findVowels(words) + " vowels in your sentence"); 
+
 //STEP 4
 
+// var amount;
+// var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+// function makeRandom (amount) { 
+//     var randomText = ""
+//     "use strict";
+//     amount = window.prompt("Pick a random number");
+//     // use the number you get from amount to iterate (amount) many times and every time you iterate you get a random number equal to the location of a character in the char_list string
+//     // YOU NEED TO ADD AND ASSING += TO THE STRING FOR randomText if you only do =, you will only get 1 number 
+//     for (var i = 0; i < amount; i++) {
+//         randomText += char_list.charAt(Math.floor(Math.random() * char_list.length));
+    
+//     } 
+//     return randomText; 
+// }
+    
+// window.console.log(makeRandom(amount));
+
+
 //STEP 5
+
+// var countryNames = ["Australia", "Germany", "Nepal", "United States of America"]; 
+
+// function longestCountry() {
+//     "use strict";
+    
+    
+// }
+
+
+var countryNames = ("Australia, Germany, Nepal, United States of America"); 
+var newCountries = countryNames.split(",");
+window.console.log(newCountries); 
+function counter () {
+    var  countryArray;
+for (var i = 0; i < newCountries.length; i++) {
+    countryArray += newCountries[i].length;
+    return countryArray;
+}
+
+}
+
+window.console.log(counter(newCountries)); 
+
+
+
+
--- js/math.js
+++ js/math.js
@@ -3,9 +3,11 @@
 //STEP 1
 // var num = window.prompt("Pick a number");
 // window.console.log(Math.abs(num));
+
 //STEP 2
 // var num = window.prompt("Pick a floating point value (a decimal)");
 // window.console.log("Rounding up " + num + " you get " + Math.ceil(num));
+
 //STEP 3
 // var num = window.prompt("Pick a floating point value (a decimal)");
 // window.console.log("Rounding down " + num + " you get " + Math.floor(num));
@@ -17,7 +19,6 @@ var num2 = num.split(",");
 
 window.console.log("The largest number in that list is: " + Math.max.apply(null, num2)); // use .apply(null, num2) to pass the  new array into the math object 
 window.console.log("The smallest number in that list is: "+ Math.min.apply(null, num2)); // use .apply(null, num2) to pass the  new array into the math object 
-
 
 
 
